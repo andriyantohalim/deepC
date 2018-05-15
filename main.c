72dd665c5f4296cf5189c0c94bbe75dd372dc069
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-05-15 13:44:44 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -14,51 +14,50 @@ int main(void)
 	// record currect clock as start point
 	start = clock();
 	
+	Tensor AAA = random_tensor_normalized(448,448, 3);
 	
-	Tensor AAA = random_tensor(448,448, 3);
-	
-	Tensor kkk0 = random_tensor(7,7, 3);
+	Tensor kkk0 = random_tensor_normalized(7,7, 3);
 	Tensor BBB = convolution_2d_Tsr_wCPU(&AAA, &kkk0, 3, 5);
 	Tensor BBBo = Leaky_ReLU_Tsr_wCPU(&BBB);
 	
-	Tensor kkk1 = random_tensor(5,5, 5);
+	Tensor kkk1 = random_tensor_normalized(5,5, 5);
 	Tensor BBBn = normalization_Tsr_wCPU(&BBBo);
 	Tensor CCC = convolution_2d_Tsr_wCPU(&BBBn, &kkk1, 2, 5);
 	Tensor CCCo = Leaky_ReLU_Tsr_wCPU(&CCC);
 	
-	Tensor kkk2 = random_tensor(5,5, 5);
+	Tensor kkk2 = random_tensor_normalized(5,5, 5);
 	Tensor CCCn = normalization_Tsr_wCPU(&CCCo);
 	Tensor DDD = convolution_2d_Tsr_wCPU(&CCCn, &kkk2, 2, 4);
 	Tensor DDDo = Leaky_ReLU_Tsr_wCPU(&DDD);
 	
-	Tensor kkk3 = random_tensor(3,3, 4);
+	Tensor kkk3 = random_tensor_normalized(3,3, 4);
 	Tensor DDDn = normalization_Tsr_wCPU(&DDDo);
 	Tensor EEE = convolution_2d_Tsr_wCPU(&DDDn, &kkk3, 1, 4);
 	Tensor EEEo = Leaky_ReLU_Tsr_wCPU(&EEE);
 	
-	Tensor kkk4 = random_tensor(3,3, 4);
+	Tensor kkk4 = random_tensor_normalized(3,3, 4);
 	Tensor EEEn = normalization_Tsr_wCPU(&EEEo);
 	Tensor FFF = convolution_2d_Tsr_wCPU(&EEEn, &kkk4, 1, 5);
 	Tensor FFFo = Leaky_ReLU_Tsr_wCPU(&FFF);
 	
-	Tensor kkk5 = random_tensor(3,3, 5);
+	Tensor kkk5 = random_tensor_normalized(3,3, 5);
 	Tensor FFFn = normalization_Tsr_wCPU(&FFFo);
 	Tensor GGG = convolution_2d_Tsr_wCPU(&FFFn, &kkk5, 2, 5);
 	Tensor GGGo = Leaky_ReLU_Tsr_wCPU(&GGG);
 	
-	Tensor kkk6 = random_tensor(3,3, 5);
+	Tensor kkk6 = random_tensor_normalized(3,3, 5);
 	Tensor GGGn = normalization_Tsr_wCPU(&GGGo);
 	Tensor HHH = convolution_2d_Tsr_wCPU(&GGGn, &kkk6, 1, 10);
 	Tensor HHHo = Leaky_ReLU_Tsr_wCPU(&HHH);
 	
-	Tensor kkk7 = random_tensor(3,3, 10);
+	Tensor kkk7 = random_tensor_normalized(3,3, 10);
 	Tensor HHHn = normalization_Tsr_wCPU(&HHHo);
 	Tensor III = convolution_2d_Tsr_wCPU(&HHHn, &kkk7, 1, 100);
 	Tensor IIIo = Leaky_ReLU_Tsr_wCPU(&III);	
 	
 	Vector I = Tsr2Vec_wCPU(&IIIo);
 
-	Matrix WW0 = random_matrix(600, I.len);
+	Matrix WW0 = random_matrix_normalized(600, I.len);
 	Vector B0 = constant_vector(600, 0.000523f);
 	 
 	Vector J = Fully_Connected_wCPU(&I, &WW0, &B0);
@@ -71,7 +70,7 @@ int main(void)
 	Vector Ko = sigmoid_Vec_wCPU(&K);
 	
 	Matrix WW2 = constant_matrix(5, 30, 0.000415f);
-	Vector B2 = random_vector(5);
+	Vector B2 = random_vector_normalized(5);
 	
 	Vector L = Fully_Connected_wCPU(&Ko, &WW2, &B2);
 	Vector Lo = softmax_Vec_wCPU(&L);
